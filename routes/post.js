7ec8827c0ec0db02ce036c46e5e3530d40b78a6c
Lavish <lavishjain36@gmil.com>
2022-11-16 20:18:00 +0530

    add

--- routes/post.js
+++ routes/post.js
@@ -21,6 +21,7 @@ router.get('/allpost',(req,res)=>{
 
 router.post('/createpost',requireLogin,(req,res)=>{
 const {title,body,pic}=req.body
+console.log(title,body,pic)
 if(!title||!body||!pic){
     return res.status(422).json({error:"Please add all fields"})
 }
